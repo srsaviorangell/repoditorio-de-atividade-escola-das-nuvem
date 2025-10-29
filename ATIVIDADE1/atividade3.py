62f689c427bf1831edc965ec183c0cea9e4ebb6d
savio rangel souza costa <sr188bet@gmail.com>
2025-10-29 19:49:51 -0300

    atividade1

---
+++ ATIVIDADE1/atividade3.py
@@ -0,0 +1,5 @@
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print(f"O volume da caixa é {volume} cm³")
---
+++ ATIVIDADE1/atividade5.py
@@ -0,0 +1,6 @@
+A = 5
+B = 6
+C = 7
+D = 8
+DIFERENCA = (A * B - C * D)
+print(f"DIFERENCA = {DIFERENCA}")
